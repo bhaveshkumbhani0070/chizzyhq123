d5b1c5400330a7c71c84ac5c8fa2086b75e1439d
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2017-12-16 17:14:18 +0530

    [WP] UPDATE FOR SAVE DATA
    Update for save data into server data base

--- config/db.js
+++ config/db.js
@@ -18,10 +18,10 @@ if (!database) {
     console.log('Please enter database');
 }
 var config = {
-    user: process.env.user, // 'holiday',
-    password: process.env.password, //'97762fc4a786331a90f044485ecd11c47acced473e460191',
-    server: process.env.server, //'holidaypackages.corkidnfytiw.ap-southeast-2.rds.amazonaws.com',
-    database: process.env.database //'holidaypackage'
+    user: process.env.user,
+    password: process.env.password,
+    server: process.env.server,
+    database: process.env.database
 };
 const pool = new sql.ConnectionPool(config)
 
