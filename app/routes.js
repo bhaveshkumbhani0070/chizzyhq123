2d9790bad3a44ce47a27a93c56a2755cf9099d7f
srjahir32 <srjahir32@gmail.com>
2017-12-23 09:56:41 +0530

    update for api

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     app.get('/holiday', function(req, res) {
         res.sendfile('./public/holidaypackage.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
-    app.get('/admin/scrape/flightcentre', flightcentre.scrape);
+    app.get('/admin/scrape/flightcentre/:l', flightcentre.scrape);
     app.post('/api/getallDeal', api.getallDeal);
     app.get('/api/getDeparture', api.getDeparture);
     app.get('/api/getDestination/:destination', api.getDestination);
