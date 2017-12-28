ca9cb46aa5e9cfd11ed867919a4fd3c591bd2573
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2017-12-28 23:44:34 +0530

    [WP] CREATE SCRAPE CODE FOR HOLIDAYCENTRE SITE

--- scrape/myholidaycentre.js
+++ scrape/myholidaycentre.js
@@ -32,6 +32,7 @@ function getAllUrl() {
 
                 if (data.attr("href").includes('http:')) {
                     console.log('this', data.attr("href"));
+                    //getChildData(data.attr("href"));
                 } else {
                     console.log('Other Urls', data.attr("href"));
                 }
@@ -41,8 +42,15 @@ function getAllUrl() {
         }
     });
 }
-var child = "http://myfiji.com";
-// getChildData(child);
+// pool.close();
+// pool.connect(function(err, connection) {
+//     if (!err) {
+//         var child = "http://myfiji.com";
+//         getChildData(child);
+//     } else {
+
+//     }
+// });
 
 function getChildData(child) {
     request(child, function(error, response, html) {
@@ -50,14 +58,60 @@ function getChildData(child) {
             var $ = cheerio.load(html);
             $('.et_pb_text_inner .element-item .item-tile a').each(function(i, e) {
                 var data = $(this);
-                var url = data.attr("href");
-                var details = data.find('.details');
+                var allData = {};
+                var link = data.attr("href")
+                allData["link"] = link;
+                allData["title"] = data.find('.details h3').text();
+                var NightPrice = data.find('.details .small').text().split('$');
+                allData["nights"] = parseInt(NightPrice[0]);
+                allData["price"] = parseInt(NightPrice[1]);
 
-                console.log('details', details);
-                //  console.log('this', data.attr("href"));
+                setTimeout(function(z) {
+                    requ.query("select * from deal where link='" + link + "'", function(err, cityD) {
+                        if (!err) {
+                            if (cityD.recordset.length > 0) {
+                                console.log('Already there', link)
+                            } else {
+                                ScrapeFromInner(allData, link, z);
+                            }
+                        } else {
+                            console.log('Error for select data from deal table', err);
+                        }
+                    });
+                }, i * 3500, i);
             })
         } else {
             console.log('Error', err);
+        }
+    });
+}
+
+var allData = {
+        link: "https://www.myfiji.com/package/sheraton-fiji-resort-ocean-view-room-7-nights-flash-sale/"
+    }
+    //ScrapeFromInner(allData, 0);
+
+function ScrapeFromInner(allData, z) {
+    request(allData.link, function(error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            var departure = [];
+            $('#departure-iata option').each(function(i, e) {
+                var depa = $(this);
+                var price = depa.attr('data-price');
+                var value = depa.text();
+                departure.push({
+                    price: price,
+                    value: value
+                })
+            })
+            allData["departure"] = departure;
+
+            var purchase_by = $('.book-date .date').text();
+            purchase_by = purchase_by.split('Book by')[1].trim();
+            allData["purchase_by"] = getDate(purchase_by)
+        } else {
+            console.log('Error for scrape from inner', error);
         }
     });
 }
@@ -69,10 +123,10 @@ function getChildData(child) {
 
 
 
-
-
-
-
+function getDate(date) {
+    var d = new Date(date);
+    return d.getMonth() + 1 + '/' + d.getDate() + '/' + d.getFullYear();
+}
 
 /**
  * DD/MM/YYYY
