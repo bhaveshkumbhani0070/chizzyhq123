dd91b74a49593b2952229b783efe7366601138b8
srjahir32 <srjahir32@gmail.com>
2017-12-28 18:12:37 +0530

    Update for scrape new site

--- scrape/myholidaycentre.js
+++ scrape/myholidaycentre.js
@@ -1,4 +1,12 @@
-var url = "https://www.myholidaycentre.com.au/";
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+var util = require('util');
+var sql = require('mssql');
+//var connection = require(__dirname + '/config/db');
+var pool = require('../config/db');
+const requ = new sql.Request(pool);
+
 
 
 //  deal(description,destination,stars,nights,link,title,purchase_by,agency)
@@ -12,12 +20,65 @@ var url = "https://www.myholidaycentre.com.au/";
 
 exports.holidaycenterScrape = function(req, res) {
 
-    }
-    /**
-     * DD/MM/YYYY
-     *  TO
-     * MM/DD/YYYY
-     */
+}
+
+function getAllUrl() {
+    var url = "https://www.myholidaycentre.com.au/";
+    request(url, function(error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            $('.item-tile a').each(function(i, e) {
+                var data = $(this);
+
+                if (data.attr("href").includes('http:')) {
+                    console.log('this', data.attr("href"));
+                } else {
+                    console.log('Other Urls', data.attr("href"));
+                }
+            })
+        } else {
+            console.log('Error', err);
+        }
+    });
+}
+var child = "http://myfiji.com";
+// getChildData(child);
+
+function getChildData(child) {
+    request(child, function(error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            $('.et_pb_text_inner .element-item .item-tile a').each(function(i, e) {
+                var data = $(this);
+                var url = data.attr("href");
+                var details = data.find('.details');
+
+                console.log('details', details);
+                //  console.log('this', data.attr("href"));
+            })
+        } else {
+            console.log('Error', err);
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * DD/MM/YYYY
+ *  TO
+ * MM/DD/YYYY
+ */
 function purchageDate(date) {
     if (date) {
         var newD = date.split("/");
