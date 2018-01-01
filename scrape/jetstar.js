a32a1e049b9ab9610913b0d99f1fe2452fc10e3e
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2018-01-01 08:30:47 +0530

    Updatte for jetstar web site

--- scrape/jetstar.js
+++ scrape/jetstar.js
@@ -1,3 +1,36 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+var util = require('util');
+var sql = require('mssql');
+//var connection = require(__dirname + '/config/db');
+var pool = require('../config/db');
+const requ = new sql.Request(pool);
+
+
 exports.jetstarScrape = function(req, res) {
 
+}
+
+var url = "http://www.jetstar.com/au/en/holidays/sunshine-coast";
+// startScrape(url);
+
+function startScrape(ul) {
+    request(url, function(error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            $('.product-section__btn').each(function(e) {
+                var data = $(this);
+
+                if (data.attr("href").includes('http:')) {
+                    var link = data.attr('href');
+                } else {
+                    var link = "http://www.jetstar.com" + data.attr('href');
+                }
+                console.log('data', link);
+            })
+        } else {
+            console.log('Errror', error);
+        }
+    })
 }
