212f512898dac633524ce594da33b3ae5708254c
kumbhanialex111 <kumbhani.bhavesh.1@gmail.com>
2018-01-02 08:35:48 +0530

    Update for jetstar scrape from post url

--- scrape/jetstar.js
+++ scrape/jetstar.js
@@ -61,10 +61,30 @@ function childData(link) {
     request(link, function(err, response, html) {
         if (!err) {
             var $ = cheerio.load(html);
-            $('.packages-wrapper .package-card--x2').each(function(e) {
+            $('.packages-wrapper .grid .js-static-package').each(function(e) {
                 var data = $(this);
-                console.log('data', $('.package-card__description'));
+                var allData = [];
+                allData["title"] = data.find('.package-card__holiday-name').text();
+                allData["destination"] = data.find('.package-card__destination').text().trim();
+                var stars = data.find('.rating__text').text().split('out of')[0]
+                stars = stars.trim().split('-')[1];
+                allData["stars"] = parseInt(stars);
+
+                // data.find('.package-card__prices .package-card__price').each(function(e) {
+                //     var data = $(this);
+                //     var allData = [];
+                //     allData["title"]=data.attr('data-package-name');
+                //     allData["destination"]=data.attr('data-destination-name');
+                //     allData["price"]=data.attr('data-package-price');
+
+                //     console.log('data', data.attr('data-package-price'));
+                // })
             });
         }
     });
 }
+
+
+// deal(description,,stars,nights,link,,purchase_by,agency   destination,title)
+// deal_departure(deal_id,departure,price)
+// deal_dates(deal_id,deal_departure_id,date_from,date_to)
