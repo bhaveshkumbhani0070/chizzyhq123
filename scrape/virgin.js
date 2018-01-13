cc3571e933aff2269eb77126db7332bc05232c09
srjahir32 <srjahir32@gmail.com>
2018-01-13 11:55:29 +0530

    Update for scrape from site

--- scrape/virgin.js
+++ scrape/virgin.js
@@ -26,8 +26,13 @@ function Scrap(u, page) {
             $('.view-vah-holiday-packages-landing .view-content').children().each(function(e) {
                 var data = $(this);
                 if (!data.hasClass("views-row-first")) {
-                    // console.log('data', data.find('.card__title').text());
-                    console.log('link', data.find('.card__actions').children().attr('href'))
+
+                    var chilUrl = "https://travel.virginaustralia.com" + data.find('.card__actions').children().attr('href');
+                    for (var i = 0; i < desti.length; i++) {
+                        var url = chilUrl + "?origin_airport_nid=" + desti[i].id + "&Submit=Go&field_deal_search_type_value=flight_hotel"
+                        childData(url);
+                    }
+
                 }
             })
         } else {
@@ -35,3 +40,39 @@ function Scrap(u, page) {
         }
     })
 }
+
+var desti = [{ id: 3569, value: "Adelaide" },
+    { id: 3575, value: "Brisbane" },
+    { id: 3599, value: "Melbourne" },
+    { id: 3604, value: "Perth" },
+    { id: 3610, value: "Sydney" },
+    { id: 3578, value: "Cairns" },
+    { id: 3568, value: "Canberra" },
+    { id: 3588, value: "Gold Coast" },
+    { id: 3592, value: "Hobart" },
+]
+var u = "https://travel.virginaustralia.com/au/holidays/broome?origin_airport_nid=3569&Submit=Go&field_deal_search_type_value=flight_hotel";
+// childData(u);
+
+function childData(u) {
+    //  console.log('u', u);
+    request(u, function(error, response, html) {
+        if (!error) {
+            var $ = cheerio.load(html);
+            var data = $(this);
+            $('.view-vah-holiday-packages .view-content').children().each(function(e) {
+                var data = $(this);
+                console.log('text', data.find('.card__title').text());
+            });
+        } else {
+            console.log('Error');
+        }
+    })
+}
+
+
+// var samDes = [
+//     { departure: "Darwin", $$hashKey: "013" },
+//     { departure: "old Coast", $$hashKey: "017" },
+//     { departure: "risbane", $$hashKey: "019" }
+// ]
